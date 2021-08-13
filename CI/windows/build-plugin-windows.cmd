3b755b3cad7e8fd927e27e997440b5956f60c77d
Andrei Laperie <andrei.laperie@loupedeck.com>
2021-08-13 16:12:59 +0300

    initial mod

---
+++ CI/windows/build-plugin-windows.cmd
@@ -0,0 +1,11 @@
+echo **************************************************
+echo Building Loupedeck connector
+echo please run me from the root directory of this repo
+echo Assumption is you have run download-obs-deps.cmd, install-qt-win.cmd and prepare-obs-windows.cmd  scripts
+echo If you did not, please exit at this stage, otherwise please continue
+CI\windows\settings.cmd
+pause
+CI\windows\prepare-plugin-windows.cmd 
+cd ..
+cmake --build build32
+cmake --build build64
