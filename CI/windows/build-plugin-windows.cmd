35cc58d2f7ac5926500b2f4eeb41861406e1ce75
Andrei Laperie <andrei.laperie@loupedeck.com>
2021-08-24 15:25:38 +0300

    rename to loupedeck-obs module

---
+++ CI/windows/build-plugin-windows.cmd
@@ -0,0 +1,14 @@
+echo **************************************************
+echo Building Loupedeck connector
+echo please run me from the root directory of this repo
+echo Assumption is you have run download-obs-deps.cmd, install-qt-win.cmd and prepare-obs-windows.cmd  scripts
+echo If you did not, please exit at this stage, otherwise please continue
+echo OBS binaries also need to be build prior to that for the same buildconfig
+
+call CI\windows\settings.cmd
+pause
+
+call CI\windows\prepare-plugin-windows.cmd 
+
+cmake --build build32 --config %build_config%
+cmake --build build64 --config %build_config%
